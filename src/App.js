963e14046081893f1797fdbb0ee129129316a7df
Martin Orefice <orefice9b@gmail.com>
2022-08-25 21:04:15 -0300

    contador con boton

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { ThemeProvider } from '@mui/material';
 import './App.css';
+import ItemCount from './components/ItemCount';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import Promesas from './tests/Promesas';
 import theme from './MuiTheme';
 
 
 
 function App() {
   //Lógica
+  
+  const onAdd=()=>{
+    console.log("Item agregado al carrito");
+  }
 
   return (
     <ThemeProvider theme={theme}>
         <NavBar/>
         <ItemListContainer greeting='¡Bienvenidos a mi tienda online!'/>
+        <ItemCount valorInicial={1} stock={5} onAdd={onAdd}/>
+        <Promesas/>
     </ThemeProvider>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+const ItemCount = ({valorInicial, stock, onAdd}) => {
+
+    const [counter, setCounter] = useState(valorInicial)
+
+    const sumar= ()=>{
+      if(counter < stock){
+      setCounter(counter+1)
+      }
+    }
+
+    const restar= ()=>{
+      if(counter > 1){ 
+      setCounter(counter-1)
+      }
+    }
+
+    //const addToCart=()=>{
+    //  console.log(counter);
+    //} 
+
+  return (
+    <div>
+        <button style={{marginLeft: 10, fontSize: 20}} onClick={sumar}>+</button>
+        <span style={{marginLeft: 10, fontSize: 20}}> {counter} </span>
+        <button style={{marginLeft: 10, fontSize: 20}} onClick={restar}>-</button>
+        <button style={{marginLeft: 10, fontSize: 20}} onClick={onAdd}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
